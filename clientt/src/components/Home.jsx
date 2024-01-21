0e05ea11ad12d4f83ef3d3c10ca6cf3ffd1cdbfa
pravin nandankar <pravinnandankar03@gmail.com>
2024-01-21 12:08:22 +0530

    Navbar

---
+++ clientt/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='w-full absolute top-0 -z-10 h-screen'>
+        {/* <img className='w-screen h-screen bg-cover' src="https://dataconomy.com/wp-content/uploads/2023/05/Best-AI-photo-editor-Free-online-and-more-1-12.jpg" alt="" /> */}
+       <h1>Nike Shoes</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ clientt/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { FaInstagram } from 'react-icons/fa6';
+// import { CiLinkedin } from 'react-icons/ci';
+// import { FaGithub } from 'react-icons/fa';
+// import { SiCodeproject, SiCodechef } from 'react-icons/si';
+import { IoCartOutline } from "react-icons/io5";
+import { IoMdSearch } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
+
+function Navbar() {
+    const [bar,setbar] = useState(false)
+    function opensearch() {
+      setbar(!bar)
+    }
+    return (
+
+        <div className='w-full flex justify-center items-center' >
+          <div className='w-full flex flex-col justify-center items-center'>
+            <div className='w-[70%]  border shadow-xl p-7 mt-8 flex items-center justify-between'>
+                <div>
+                    <h1>Logo</h1>
+                </div>
+                <div className='flex gap-8 justify-center items-center'>
+                    <Link className='hover:text-orange-600' to='/' >Home</Link>
+                    <Link className='hover:text-orange-600' to='/shop' >Shop</Link>
+                    <Link className='hover:text-orange-600' to='/' >Blog</Link>
+                    <Link className='hover:text-orange-600' to='/' >Pages</Link>
+                    <Link className='hover:text-orange-600' to='/' >Contact</Link>
+                    <Link className='hover:text-orange-600' to='/' ><IoCartOutline /></Link>
+                    <Link className='hover:text-orange-600' to='/' onClick={opensearch}><IoMdSearch /></Link>
+                </div>
+            </div>
+            { bar && 
+            <div className='w-[70%] bg-orange-400 border shadow-xl p-5 flex items-center justify-between transition-all'>
+                <input className='w-[80%] bg-transparent border-b text-black border-black outline-none' placeholder='search product' type="search" name="search" id="" />
+                <span className='text-2xl cursor-pointer' onClick={opensearch}><RxCross2 /></span>
+            </div>
+              }
+          </div>
+        </div>
+    );
+}
+
+export default Navbar;
